98adf4cea6f8d55f3c2f5c0970d58d8b6b929a4e
MasatoshiTada <rosso.e.nero84@gmail.com>
2017-02-27 14:44:30 +0900

    Delete unused import

--- 02-spring-boot/src/main/java/com/example/Application.java
+++ 02-spring-boot/src/main/java/com/example/Application.java
@@ -3,8 +3,6 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.example.persistence.entity", "org.springframework.data.jpa.convert.threeten"})
